281c282294375d9990bb95854c57bf16e53187b5
Muhammad Haseeb <haseebmuhammad029@gmail.com>
2020-08-15 23:49:02 +0500

    Components part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
