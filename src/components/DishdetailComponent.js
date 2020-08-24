1c91e889970d2a9422f7d2d49144db428ae6dc03
Muhammad Haseeb <haseebmuhammad029@gmail.com>
2020-08-25 04:15:35 +0500

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Card, CardImg,  CardText, CardBody,
     Label, Col  } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required= (val) => val && val.length;
 const maxLength= (len) => (val) => !(val) || (val.length <= len);
@@ -150,9 +151,26 @@ class CommentForm extends Component{
     }
     
     const DishDetail = (props) => {
-
-    
-          return (
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null)
+            return (
                 <div className="container">
                 <div className="row">
                     <Breadcrumb>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb , BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderMenuitem ({dish, onClick})
 {
@@ -19,32 +20,51 @@ return(
 } 
 
     const Menu = (props) =>  {
-        const menu = props.dishes.map((dish) => {
+        const menu = props.dishes.dishes.map((dish) => {
             return (
                 <div  className="col-12 col-md-5 m-1" key={dish.id}> 
                   <RenderMenuitem dish={dish} onClick={props.onClick}/>
                 </div>
               );
         });
-    
-        return (
-            <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Menu</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Menu</h3>
-                    <hr />
-                </div>                
+        
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else
+            return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    {menu}
+                </div>
+                
             </div>
-            <div className="row">
-                {menu}
-            </div>
-            
-        </div>
-    );
+        );
     }
   
 export default Menu;
