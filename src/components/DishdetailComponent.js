0e03dc0274b0da269215c25fe7848aa09d475bcf
Muhammad Haseeb <haseebmuhammad029@gmail.com>
2020-08-27 02:03:00 +0500

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,7 +8,9 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
+//===================================================
 const required= (val) => val && val.length;
 const maxLength= (len) => (val) => !(val) || (val.length <= len);
 const minLength= (len) => (val) => (val) && (val.length >= len);
@@ -108,14 +110,24 @@ class CommentForm extends Component{
     function  RenderDish( {dish}){
         if(dish != null)
             return(
-            <Card>
-                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle> {dish.name}
-                    </CardTitle>
-                    <CardText>{dish.description} </CardText>
-                </CardBody>
-            </Card>
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }} >
+                    <Card>
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle> {dish.name}
+                            </CardTitle>
+                            <CardText>{dish.description} </CardText>
+                        </CardBody>
+                     </Card>
+
+
+
+                </FadeTransform>
+            
         );
        else
         return(
@@ -124,28 +136,37 @@ class CommentForm extends Component{
     }
 
     function RenderComments({comments, postComment, dishId}) {
-        
+                
                 const my_comments=comments.map((comment)=>{
                     // const date= new Date(comment.date)
                     return(
+                        <Fade in>
                         <li key={comment.id} >
-                        <p >{comment.comment} </p>
-                    <p>--{comment.author}, 
-                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                        
+                        <p >{comment.id} </p>
+                            <p >{comment.comment} </p>
+                            <p>--{comment.author}, 
+                            {new Intl.DateTimeFormat('en-US', 
+                            { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+
                         </li>   
+                        </Fade>
                     );
+                    
                 });
                 return(
-                    
                         <div>
                             <h4>Comments</h4>
                             <ul className="list-unstyled">
+                            <Stagger in>
                             {my_comments}
-                            </ul>
                             <CommentForm
                             dishId={dishId} postComment={postComment}/>    
+                            </Stagger> 
+                            </ul>
+                              
+                             
                         </div>
+                        
                    
                 );
             
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,7 +13,7 @@ import { Switch, Route, Redirect,withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos,fetchLeaders } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
-
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 const mapStateToProps = state => {
   return {
@@ -91,15 +91,21 @@ class Main extends React.Component {
     return (
       <div>
         <Header/>
-        <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-              <Route path='/menu/:dishId' component={DishWithId} />
-              <Route path='/contactus' component={()=> <Contact 
-              resetFeedbackForm={this.props.resetFeedbackForm}/>} />
-              <Route path='/aboutus' component={Aboutus}/>
-              <Redirect to="/home" />
-          </Switch>   
+
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+              <Switch>
+                  <Route path='/home' component={HomePage} />
+                  <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                  <Route path='/menu/:dishId' component={DishWithId} />
+                  <Route path='/contactus' component={()=> <Contact 
+                  resetFeedbackForm={this.props.resetFeedbackForm}/>} />
+                  <Route path='/aboutus' component={Aboutus}/>
+                  <Redirect to="/home" />
+              </Switch> 
+          </CSSTransition>
+        </TransitionGroup>
+          
         <Footer/>
       </div>
     );
