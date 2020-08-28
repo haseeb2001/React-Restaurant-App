f98c82abc11aba89727bf720855a7848389e9444
Muhammad Haseeb <haseebmuhammad029@gmail.com>
2020-08-29 04:35:46 +0500

    Updates

---
+++ precache-manifest.10d7e5c8857cb5a70fc091ddbad370f1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41025d408e6279ab1398bcf4d7318dda",
+    "url": "/React-Restaurant-App/index.html"
+  },
+  {
+    "revision": "88f860b186a2afe79807",
+    "url": "/React-Restaurant-App/static/css/2.49d7573d.chunk.css"
+  },
+  {
+    "revision": "4fe6d7b000696226b59f",
+    "url": "/React-Restaurant-App/static/css/main.bc2a2813.chunk.css"
+  },
+  {
+    "revision": "88f860b186a2afe79807",
+    "url": "/React-Restaurant-App/static/js/2.dd0335e2.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/React-Restaurant-App/static/js/2.dd0335e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe6d7b000696226b59f",
+    "url": "/React-Restaurant-App/static/js/main.abaf0686.chunk.js"
+  },
+  {
+    "revision": "4047304dc498f224deb9",
+    "url": "/React-Restaurant-App/static/js/runtime-main.befcc353.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74fcc6f27fb8f111d01c1840125e315f.js"
+  "/React-Restaurant-App/precache-manifest.10d7e5c8857cb5a70fc091ddbad370f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Restaurant-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
