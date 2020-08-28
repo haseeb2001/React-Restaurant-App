e26dc38dc05ab836b697ab3e0ba1f70ae9e5e050
Muhammad Haseeb <haseebmuhammad029@gmail.com>
2020-08-29 04:20:04 +0500

    Updates

---
+++ precache-manifest.74fcc6f27fb8f111d01c1840125e315f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdbd884490a8f6d9c255988ce8b3f1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d08d332075dc390e608",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "0b8f574dbb23d724995e",
+    "url": "/static/css/main.bc2a2813.chunk.css"
+  },
+  {
+    "revision": "3d08d332075dc390e608",
+    "url": "/static/js/2.dd0335e2.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.dd0335e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8f574dbb23d724995e",
+    "url": "/static/js/main.58d97241.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
